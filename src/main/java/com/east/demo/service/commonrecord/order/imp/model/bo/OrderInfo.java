27870ce5b38f15496b86146bbfa3a6ec6801a348
luyublog <eastcukt@gmail.com>
2023-11-24 17:08:47 +0800

    下单设计模式

--- src/main/java/com/east/demo/service/commonrecord/order/imp/model/bo/OrderInfo.java
+++ src/main/java/com/east/demo/service/commonrecord/order/imp/model/bo/OrderInfo.java
@@ -1,10 +1,18 @@
 package com.east.demo.service.commonrecord.order.imp.model.bo;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
 /**
  * 账单对应信息
  *
  * @author: east
  * @date: 2023/11/23
  */
+@Data
 public class OrderInfo {
+    protected BigDecimal amt;
+    protected String serial;
+    protected String rcvNo;
 }
