5e4cd40cd20413c3740c18ad67e46b74b0929efc
luyublog <luyublog@gmail.com>
2024-02-26 18:54:18 +0800

    #update 自定义32进制特征码生成注释

--- src/main/java/com/east/demo/service/util/common/CommonUtil.java
+++ src/main/java/com/east/demo/service/util/common/CommonUtil.java
@@ -85,14 +85,15 @@ public class CommonUtil {
     }
 
     /**
-     * 将序列打乱为8位
+     * 将序列（序列最大值不要超过32^5否则会生成重复）打乱为8位，从而防止自定义的32位进制字符被枚举出来，
+     * 打乱顺序时第一位不能变，因为要保证数字在32^5=33,554,432范围内，从而避免重复
      *
-     * @param l 序列值
+     * @param num 序列值
      * @return 打乱顺序后的序列
      */
-    private long encryptLong(long l) {
+    private long encryptLong(long num) {
         // 将序列格式化为至少8位的字符数组（不足8位用0补齐）
-        char[] originNum = String.format("%08d", l).toCharArray();
+        char[] originNum = String.format("%08d", num).toCharArray();
         StringBuilder str = new StringBuilder();
         // 将序列字符重新排列的规则
         for (Integer order : SEQ_ORDER_LIST) {
@@ -100,6 +101,29 @@ public class CommonUtil {
         }
         return Long.parseLong(str.toString());
     }
+
+    /**
+     * 对数字进行自定义32进制加密
+     *
+     * @param num 需要转换的数字
+     * @return 结果为5位字符
+     */
+    private String base32Encode(long num) {
+        StringBuilder base32Str = new StringBuilder();
+        long quotient = num;
+        // 计算自定义字符32进制结果
+        while (quotient > 0) {
+            // 获取余数
+            long remainder = quotient % 32;
+            base32Str.append(CHARS_SOURCE_RANDOM.charAt(Math.toIntExact(remainder)));
+            // 将商代入下次计算
+            quotient /= 32;
+        }
+        String result = base32Str.reverse().toString();
+        // 返回5位字符，多了截取前五位，少了用第一个字符替代
+        return StrUtil.padPre(result, 5, CHARS_SOURCE_RANDOM.charAt(0));
+    }
+
 
     public static void main(String[] args) {
         CommonUtil commonUtil = new CommonUtil(new LySequenceMapper() {
@@ -135,29 +159,17 @@ public class CommonUtil {
                 return null;
             }
         });
-        System.out.println(commonUtil.base32Encode(99999999L));
-    }
+        // 不打乱顺序会被枚举出来
+        System.out.println(commonUtil.base32Encode(0L));
+        System.out.println(commonUtil.base32Encode(1L));
+        System.out.println(commonUtil.base32Encode(2L));
+        System.out.println(commonUtil.base32Encode(3L));
 
+        // 打乱实现伪随机
+        System.out.println(commonUtil.base32Encode(commonUtil.encryptLong(0L)));
+        System.out.println(commonUtil.base32Encode(commonUtil.encryptLong(1L)));
+        System.out.println(commonUtil.base32Encode(commonUtil.encryptLong(2L)));
+        System.out.println(commonUtil.base32Encode(commonUtil.encryptLong(3L)));
 
-    /**
-     * 对数字进行自定义32进制加密
-     *
-     * @param num 需要转换的数字
-     * @return 结果为5位字符
-     */
-    private String base32Encode(long num) {
-        StringBuilder base32Str = new StringBuilder();
-        long quotient = num;
-        // 计算自定义字符32进制结果
-        while (quotient > 0) {
-            // 获取余数
-            long remainder = quotient % 32;
-            base32Str.append(CHARS_SOURCE_RANDOM.charAt(Math.toIntExact(remainder)));
-            // 将商代入下次计算
-            quotient /= 32;
-        }
-        String result = base32Str.reverse().toString();
-        // 返回5位字符，多了截取前五位，少了用第一个字符替代
-        return StrUtil.padPre(result, 5, CHARS_SOURCE_RANDOM.charAt(0));
     }
 }
